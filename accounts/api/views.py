4a3181116b208d070f7675182399003782f7b253
Keyuan125 <huangkeyuan125@gmail.com>
2023-04-23 14:25:23 -0600

    add login status api

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import viewsets
 from rest_framework import permissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from accounts.api.serializers import UserSerializer
 
 
@@ -13,4 +15,15 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all().order_by("-date_joined")
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+class AccountViewSet(viewsets.ViewSet):
+
+    @action(methods=['GET'], detail=False)
+    def login_status(self, request):
+        data = {'has_logged_in': request.user.is_authenticated}
+        if request.user.is_authenticated:
+            data['user'] = UserSerializer(request.user).data
+        return Response(data)
+
+
 
